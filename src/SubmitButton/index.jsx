3e870f43adc1f81b8a60b7c195121112d009da51
Hanneke Schoone <hanneke@burst-digital.com>
2017-03-03 17:39:30 +0100

    button styling

--- src/SubmitButton/index.jsx
+++ src/SubmitButton/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import getNested from 'get-nested';
+import CSSModules from 'react-css-modules';
+import styles from './styles.css';
 
+@CSSModules(styles)
 class SubmitButtonComponent extends React.Component {
   render() {
     const settings = getNested(() => this.props.form.settings, {});
     return (
       <div>
         {settings &&
-        <button {...settings.form_submit_attributes}>{settings.form_submit_label}</button>
+          <button styleName="button" {...settings.form_submit_attributes}>{settings.form_submit_label}</button>
         }
       </div>
     );
