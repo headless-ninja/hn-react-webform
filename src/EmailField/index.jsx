23c90c2acead30e31c596d0fe27434e2b6baeaff
Ian Wensink <ian@burst-digital.com>
2017-03-08 17:30:27 +0100

    Move email validation from general input to own component

--- src/EmailField/index.jsx
+++ src/EmailField/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
+import validator from 'validator';
 import styles from './styles.css';
 import Input from '../Input';
+import rules from '../Webform/rules';
+import RuleHint from '../RuleHint';
 
 @CSSModules(styles)
 class EmailField extends React.Component {
+  constructor(props) {
+    super(props);
+
+    Object.assign(rules, {
+      email: {
+        rule: value => validator.isEmail(value),
+        hint: value => <RuleHint key={`email_${this.key}`} hint={props.field['#validationError'] || '":value" isn\'t an Email.'} tokens={{ value }} />,
+      },
+    });
+  }
+
+  componentDidMount() {
+    this.props.webformElement.setState({
+      validations: [
+        ...this.props.webformElement.state.validations,
+        rules.email,
+      ],
+    });
+  }
+
   render() {
     return (
       <Input
--- src/Input/index.jsx
+++ src/Input/index.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
-import validator from 'validator';
 import getNested from 'get-nested';
 import styles from './styles.css';
-import rules from '../Webform/rules';
-import RuleHint from '../RuleHint';
 
 @CSSModules(styles)
 class Input extends React.Component {
-  constructor(props) {
-    super(props);
-
-    Object.assign(rules, {
-      email: {
-        rule: value => validator.isEmail(value),
-        hint: value => <RuleHint key={`email_${this.key}`} hint={props.field['#validationError'] || '":value" isn\'t an Email.'} tokens={{ value }} />,
-      },
-    });
-  }
-
-  componentDidMount() {
-    if(getNested(() => this.props.field['#type']) === 'email') {
-      this.props.webformElement.setState({
-        validations: [
-          ...this.props.webformElement.state.validations,
-          rules.email,
-        ],
-      });
-    }
-  }
-
   render() {
     return (
       <input
