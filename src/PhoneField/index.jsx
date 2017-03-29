449b7b3a24470838d3130199625ff117fac9a4e1
Ian Wensink <ian@burst-digital.com>
2017-03-29 19:16:10 +0200

    Rewrite PhoneField to be pure function

--- src/PhoneField/index.jsx
+++ src/PhoneField/index.jsx
@@ -3,22 +3,16 @@ import CSSModules from 'react-css-modules';
 import styles from './styles.pcss';
 import Input from '../Input';
 
-@CSSModules(styles)
-class PhoneField extends React.Component {
-  static propTypes = {
-    onChange: React.PropTypes.func.isRequired,
-  };
+const PhoneField = props => (
+  <Input
+    {...props}
+    type='tel'
+    styleName='phone'
+  />
+);
 
-  render() {
-    return (
-      <Input
-        {...this.props}
-        onChange={this.props.onChange}
-        type='tel'
-        styleName='phone'
-      />
-    );
-  }
-}
+PhoneField.propTypes = {
+  onChange: React.PropTypes.func.isRequired,
+};
 
-export default PhoneField;
+export default CSSModules(PhoneField, styles);
