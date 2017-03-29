787e9d91077a6b0ad18219bff7ccb189cb34913a
Ian Wensink <ian@burst-digital.com>
2017-03-29 19:20:01 +0200

    Rewrite SubmitButton to be pure function

--- src/SubmitButton/index.jsx
+++ src/SubmitButton/index.jsx
@@ -3,31 +3,28 @@ import CSSModules from 'react-css-modules';
 import styles from './styles.pcss';
 import Webform from '../Webform';
 
-@CSSModules(styles)
-class SubmitButton extends React.Component {
-  static propTypes = {
-    form: React.PropTypes.shape({
-      settings: React.PropTypes.object.isRequired,
-    }).isRequired,
-    status: React.PropTypes.string.isRequired,
-  };
+const SubmitButton = ({ form, status }) => {
+  const settings = form.settings;
+  const disabled = status === Webform.formStates.PENDING;
 
-  render() {
-    const settings = this.props.form.settings;
-    const disabled = this.props.status === Webform.formStates.PENDING;
+  return (
+    <div>
+      <button
+        styleName='button'
+        disabled={disabled}
+        {...settings.form_submit_attributes}
+      >
+        {settings.form_submit_label}
+      </button>
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <button
-          styleName='button'
-          disabled={disabled}
-          {...settings.form_submit_attributes}
-        >
-          {settings.form_submit_label}
-        </button>
-      </div>
-    );
-  }
-}
+SubmitButton.propTypes = {
+  form: React.PropTypes.shape({
+    settings: React.PropTypes.object.isRequired,
+  }).isRequired,
+  status: React.PropTypes.string.isRequired,
+};
 
-export default SubmitButton;
+export default CSSModules(SubmitButton, styles);
