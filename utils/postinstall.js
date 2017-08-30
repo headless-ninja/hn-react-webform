1c2dac030aa25879056c6b94f399bf7f04c1545f
Bart Langelaan <langelaanbart@gmail.com>
2017-08-30 18:42:11 +0200

    build: split up installing devDependencies and building

--- utils/postinstall.js
+++ utils/postinstall.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-console,global-require */
 const spawn = require('child_process').spawnSync;
+const path = require('path');
 
-if(!require('fs').existsSync('./lib')) {
-  console.log('Installing devDependencies of hn-react-webform..');
+if(!require('fs').existsSync(path.join(__dirname, '../node_modules/eslint'))) {
+  console.log('Installing devDependencies of hn-react-webform..', path.join(__dirname, 'node_modules/eslint/package.json'));
   spawn('npm', 'install --ignore-scripts'.split(' '), { cwd: __dirname, stdio: 'inherit' });
+} else {
+  console.log('DevDependencies of hn-react-webform already installed, not installing again.');
+}
 
+if(!require('fs').existsSync(path.join(__dirname, '../lib'))) {
   console.log('Building hn-react-webform..');
   spawn('npm', 'run build'.split(' '), { cwd: __dirname, stdio: 'inherit' });
 
