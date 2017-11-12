82d11fa222a85a96cf940f42e262846ff8bb6d10
Ian Wensink <ian@burst-digital.com>
2017-11-12 23:29:55 +0100

    feat(styled-components): fix checkbox line-height

--- src/CheckboxField/styled/inner-label.js
+++ src/CheckboxField/styled/inner-label.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export default styled.span`
   display: inline-block;
   padding-left: calc(${p => p.theme.spacingUnitCheckbox} * 1.5);
+  line-height: 1.5;
+  top: -0.2em;
+  position: relative;
 `;
