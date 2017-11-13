f442e89f863a34149490cb9d5d3dbc89447f457b
Ian Wensink <ian@burst-digital.com>
2017-11-13 08:30:15 +0100

    feat(styled-components): fix ibantools error

--- src/IBAN/index.jsx
+++ src/IBAN/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import iban from 'ibantools';
+import { isValidIBAN } from 'ibantools';
 import { observer } from 'mobx-react';
 import { site } from 'hn-react';
 import Input from '../Input';
@@ -28,7 +28,7 @@ class IBAN extends Component {
     rules.delete(`pattern_${props.field['#webform_key']}`); // IBAN has own validation, pattern is only used in back end.
 
     rules.set(`iban_${props.field['#webform_key']}`, {
-      rule: value => iban.isValidIBAN(value),
+      rule: value => isValidIBAN(value),
       hint: () =>
         <RuleHint key={`iban_${props.field['#webform_key']}`} hint={WebformUtils.getCustomValue(props.field, 'ibanError', props.settings) || site.t('Please enter a valid IBAN.')} />,
       shouldValidate: field => field.isBlurred,
