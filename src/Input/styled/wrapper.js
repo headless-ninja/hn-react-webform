83329385fdf6be933bbed0f49df20f6ebfdc3c0c
hanneke <hanneke@burst-digital.com>
2018-02-26 13:50:28 +0100

    fix(theme-settings) use inlineLabelWidth setting instead of hardcoded values

--- src/Input/styled/wrapper.js
+++ src/Input/styled/wrapper.js
@@ -4,14 +4,14 @@ export default styled.div`
   display: inline-block;
   
   ${p => p.labelDisplay === 'inline' && `
-    // width: ${p.theme.inputWidth};
+    width: ${p.theme.inputWidth};
   `}
 
   ${p => p.labelDisplay === 'before' && `
     width: ${p.theme.inputWidth};
 
     @media (min-width: 768px) {
-      width: calc(50% + ${p.theme.inputWidth});
+      width: calc(${p.theme.inlineLabelWidth} + ${p.theme.inputWidth});
     }
   `}
 `;
--- src/RadioField/styled/wrapper.js
+++ src/RadioField/styled/wrapper.js
@@ -4,7 +4,7 @@ export default styled.div`
   @media (min-width: 768px) {
     display: inline-block;
     float: left;
-    width: calc(50% - (${p => p.theme.spacingUnit} / 2) - 0.5em);
+    width: calc(${p => p.theme.inlineLabelWidth} - (${p => p.theme.spacingUnit} / 2) - 0.5em);
   }
   
   ${p => p.labelDisplay === 'before' && `
