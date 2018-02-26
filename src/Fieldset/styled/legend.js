cec20653d9ff98c10af47f7df6530d6332b8ee67
hanneke <hanneke@burst-digital.com>
2018-02-26 16:16:23 +0100

    fix(fieldset) fix inline label display styles (ie. for radio group legend)

--- src/Fieldset/styled/legend.js
+++ src/Fieldset/styled/legend.js
@@ -8,4 +8,21 @@ export default styled(Legend)`
   float: left;
   padding: 0;
   margin-bottom: calc(${p => p.theme.spacingUnitFieldset} / 2);
+  
+  ${p => p.labelDisplay === 'inline' && `
+    @media (min-width: 768px) {
+      width: ${p.theme.inlineLabelWidth};
+      padding-right: calc(${p.theme.spacingUnit} / 2);
+    }
+  `}
+
+  ${p => p.labelDisplay === 'invisible' && `
+    display: none;
+  `}
+
+  ${p => p.labelDisplay === 'before' && `
+    display: block;
+    float: none;
+    width: 100%;
+  `}
 `;
