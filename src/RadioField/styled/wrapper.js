0b95757ba769c0a5b09d368ec4390415ec083d77
hanneke <hanneke@burst-digital.com>
2018-02-27 11:36:08 +0100

    fix(radio): limit width only for inline display

--- src/RadioField/styled/wrapper.js
+++ src/RadioField/styled/wrapper.js
@@ -4,8 +4,11 @@ export default styled.div`
   @media (min-width: 768px) {
     display: inline-block;
     float: left;
-    width: calc(${p => p.theme.inlineLabelWidth} - (${p => p.theme.spacingUnit} / 2) - 0.5em);
   }
+  
+  ${p => p.labelDisplay === 'inline' && `
+    width: calc(${p.theme.inlineLabelWidth} - (${p.theme.spacingUnit} / 2) - 0.5em);
+  `}
   
   ${p => p.labelDisplay === 'before' && `
     float: none;
